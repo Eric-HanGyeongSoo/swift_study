889cc27f242ef969c999c65645610ec0355bf6ee
jbhus <gksrudtn99@gmail.com>
2021-09-06 12:41:58 +0900

    PageControl 프로젝트

---
+++ PageControl/PageControl/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by 한경수 on 2021/09/06.
+//
+
+import UIKit
+
+var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.green
+        imgView.image = UIImage(named: images[0])
+    }
+
+
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+}
+
